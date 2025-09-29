25d2629fc112c005a084067b0627a2ff1dbbeb86
Matheus <matheus.contato.6331@gmail.com>
2025-09-29 20:51:02 -0300

    Minor click function on dropdown menu

--- src/index.js
+++ src/index.js
@@ -5,8 +5,19 @@ userPic.addEventListener("click", (e) => {
     
     if(dropdownMenu.style.display == 'none') {
         dropdownMenu.style.display = 'block'
+
+        /* função de click nas opções do menu */
+        let userConfig = document.getElementById('dropdown-option1')
+        let themeConfig = document.getElementById('dropdown-option2')
+
+        userConfig.addEventListener("click", (e) => {
+            console.log('UserConfig')
+        })
+
+        themeConfig.addEventListener("click", (e) => {
+            console.log('ThemeConfig')
+        })
     }else{
         dropdownMenu.style.display = 'none'
-    }
-    
+    }  
 })
